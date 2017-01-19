d4a5b75b751644e903f99126b0ed32c7f29b6f78
R4924 <R4924@lab.aca>
2017-01-18 20:36:29 -0500

    Renderizamos vistas html con angular

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,13 @@
 'use strict';
 
 var app = angular.module("myAngularRailsApp", ['ngRoute', 'oc.lazyLoad']);
+
+app.config(function($routeProvider){
+    $routeProvider.
+	    when('/notas', { 
+	        templateUrl: 'views/nota/index.html' ,
+	    }).
+	    when('/usuarios', { 
+	        templateUrl: 'views/usuario/index.html' //,controller: 'IndexNoteCtrl'
+	    })
+});
